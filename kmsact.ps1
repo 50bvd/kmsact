47325aced12ec6d2bfbf5d27efd9266a173156cd
50bvd <loup.rks@bk.ru>
2023-01-10 16:08:06 +0100

    ASCII Art color changed

--- kmsact.ps1
+++ kmsact.ps1
@@ -8,13 +8,13 @@ $WindowsKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
 $OfficeKey = "FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH"
 $KMSserver = "kms.digiboy.ir"
 
-Write-Output "
+Write-Host "
   ___                       ___  
  (o o)                     (o o) 
 (  V  ) MS KMS Activation (  V  )
 --m-m-----------------------m-m--
     https://github.com/50bvd
-"
+" -ForegroundColor Green
 
 $Answer = Read-Host "Do you want to install Windows key, Office key or both? (W/O/B)"
 
