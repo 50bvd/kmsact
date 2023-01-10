3b8522eaa8bee9ccda5c49ea08a08023a46ce38d
50bvd <loup.rks@bk.ru>
2023-01-10 12:41:01 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# kmsact
-Windows/Office activator
+# ![pwsh](/icon/powershell.png) Windows/Office KMS Activator
+## This script will activate your Windows and Office product using KMS activation
+
+# To execute the script
+1. Download kmsact.ps1
+2. right click and run with powershellRight Click and execute with PowerShell
+3. wait a few seconds before activation
+   
---
+++ kmsact.ps1
@@ -0,0 +1,39 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    exit
+}
+
+$WindowsKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
+$OfficeKey = "FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH"
+$KMSserver = "kms.digiboy.ir"
+
+Write-Output "
+  ___                       ___  
+ (o o)                     (o o) 
+(  V  ) MS KMS Activation (  V  )
+--m-m-----------------------m-m--
+    https://github.com/50bvd
+"
+
+$Answer = Read-Host "Do you want to install Windows key, Office key or both? (W/O/B)"
+
+if ($Answer -eq "W") {
+    slmgr /ipk $WindowsKey
+    slmgr /skms $KMSserver
+    slmgr /ato
+} elseif ($Answer -eq "O") {
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /act
+} elseif ($Answer -eq "B") {
+    slmgr /ipk $WindowsKey
+    slmgr /skms $KMSserver
+    slmgr /ato
+
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
+    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /act
+} else {
+    Write-Host "Invalid answer, please type W, O or B."
+}
