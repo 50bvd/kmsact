4cfb72ca2f199e112483486acaa692ae1a749357
50bvd <loup.rks@bk.ru>
2023-04-10 22:50:12 +0200

    Add function to rearm office GVLK key

--- kmsact.ps1
+++ kmsact.ps1
@@ -1,5 +1,5 @@
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    $arguments = "& {Set-ExecutionPolicy Bypass -Scope Process -Force; & '" + $myinvocation.mycommand.definition + "'}"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     exit
 }
@@ -16,6 +16,13 @@ Write-Host "
     https://github.com/50bvd
 " -ForegroundColor Green
 
+function Initialize-OfficeLicense {
+    Push-Location
+    Set-Location "C:\Program Files\Microsoft Office\Office16"
+    cmd.exe /c "for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:""..\root\Licenses16\%x"""
+    Pop-Location
+}
+
 $Answer = Read-Host "Do you want to install Windows key, Office key or both? (W/O/B)"
 
 if ($Answer -eq "W") {
@@ -23,21 +30,19 @@ if ($Answer -eq "W") {
     slmgr /skms $KMSserver
     slmgr /ato
 } elseif ($Answer -eq "O") {
-    for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
-    cscript ospp.vbs /unpkey:6F7TH >nul
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /act
+    Initialize-OfficeLicense
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act
 } elseif ($Answer -eq "B") {
     slmgr /ipk $WindowsKey
     slmgr /skms $KMSserver
     slmgr /ato
 
-    for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
-    cscript ospp.vbs /unpkey:6F7TH >nul
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
-    cscript "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" /act
+    Initialize-OfficeLicense
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
+    cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act
 } else {
     Write-Host "Invalid answer, please type W, O or B."
 }
