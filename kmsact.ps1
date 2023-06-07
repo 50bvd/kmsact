c50b5b4262b95a181dca5174602d74687106e89a
loup.rks@bk.ru <loup.rks@bk.ru>
2023-06-08 00:12:43 +0200

    Update kmsact.ps1
    Add function to rearm office GVLK

--- kmsact.ps1
+++ kmsact.ps1
@@ -1,13 +1,15 @@
+#adm exec verif
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     $arguments = "& {Set-ExecutionPolicy Bypass -Scope Process -Force; & '" + $myinvocation.mycommand.definition + "'}"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     exit
 }
-
+#var
 $WindowsKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
 $OfficeKey = "FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH"
 $KMSserver = "50bvd.com"
-
+$Answer = Read-Host "Do you want to install Windows key, Office key or both? (W/O/B)"
+#wonderfull ascii art
 Write-Host "
   ___                       ___  
  (o o)                     (o o) 
@@ -15,26 +17,24 @@ Write-Host "
 --m-m-----------------------m-m--
     https://github.com/50bvd
 " -ForegroundColor Green
-
+#rearm office gvlk
 function Initialize-OfficeLicense {
     Push-Location
     Set-Location "C:\Program Files\Microsoft Office\Office16"
     cmd.exe /c "for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:""..\root\Licenses16\%x"""
     Pop-Location
 }
-
-$Answer = Read-Host "Do you want to install Windows key, Office key or both? (W/O/B)"
-
-if ($Answer -eq "W") {
+#menu
+if ($Answer -eq "W") { #windows act
     slmgr /ipk $WindowsKey
     slmgr /skms $KMSserver
     slmgr /ato
-} elseif ($Answer -eq "O") {
+} elseif ($Answer -eq "O") { #office act
     Initialize-OfficeLicense
     cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /inpkey:$OfficeKey
     cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /sethst:$KMSserver
     cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act
-} elseif ($Answer -eq "B") {
+} elseif ($Answer -eq "B") { #full act
     slmgr /ipk $WindowsKey
     slmgr /skms $KMSserver
     slmgr /ato
